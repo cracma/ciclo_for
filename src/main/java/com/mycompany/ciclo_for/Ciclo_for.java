1fc3d76e12bd7046e231fc7a1aaff39dcd614a2e
Marco <cracma@gmail.com>
2024-11-18 08:34:46 -0500

    Inicializando repositorio

---
+++ src/main/java/com/mycompany/ciclo_for/Ciclo_for.java
@@ -0,0 +1,19 @@
+package com.mycompany.ciclo_for;
+
+import java.util.Scanner;
+
+public class Ciclo_for {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+       
+        System.out.println("Cantidad de repeticion ");
+        int can = entrada.nextInt();
+        
+        for (int i = 1; i <= can; i++) {
+            System.out.println("Dato numero "+i);
+        }
+       
+        
+    }
+}
